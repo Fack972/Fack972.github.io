b8a3138e08cf80128072209e14bd0173d679d056
Fack972 <36539280+Fack972@users.noreply.github.com>
2018-03-28 16:00:07 +0200

    ggle analytics
    rajout d'un filtre pour analyse de fréquentation

--- send.php
+++ send.php
@@ -7,11 +7,10 @@
      
     $headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME
     $headers .= 'Content-type: text/html; charset=ISO-8859-1'."\n"; // l'en-tete Content-type pour le format HTML
-    $headers .= 'To: '.$destinataire."\n"; // Mail de reponse
+    /*$headers .= 'To: '.$destinataire."\n"; // Mail de reponse*/
     $headers .= 'From: "From CV online"<'.$expediteur.'>'."\n"; // Expediteur
      
-    $message =  '<div style="width: 100%; text-align: center; font-weight: bold"> Bonjour '.$_POST['name'].'!<br>
-                    '.$_POST['message'].'</div>';
+    $message =  '<div style="width: 100%; text-align: center; font-weight: bold"> '.$_POST['message'].'</div>';
      
     if(mail($destinataire, $objet, $message, $headers))
     {
